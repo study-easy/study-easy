726d38de1ccd0b1a5365a23f4befddf210387a9c
SirBattle3 <tobias.94@gmx.de>
2015-06-25 12:18:30 +0200

    StartProjekt
    StratProjekt

---
+++ study-easy/src/main/java/org/hohenheim/GreetingController.java
@@ -0,0 +1,21 @@
+package org.hohenheim;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class GreetingController {
+	@RequestMapping(method=RequestMethod.GET)
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	model.addAttribute("name", name);
+	//System.out.println(name);
+	return"index";}
+
+
+	
+}
+
