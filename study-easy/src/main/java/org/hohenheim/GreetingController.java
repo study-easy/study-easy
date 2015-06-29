f55b1aeec050e43dffba9500d7c101e363f367e5
SirBattle3 <tobias.94@gmx.de>
2015-06-29 23:41:26 +0200

    s
    s

--- study-easy/src/main/java/org/hohenheim/GreetingController.java
+++ study-easy/src/main/java/org/hohenheim/GreetingController.java
@@ -1,14 +1,36 @@
 package org.hohenheim;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+import Users.RegUser;
 
 @Controller
 @RequestMapping("/")
 public class GreetingController { 
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/test3").setViewName("test3");
+    }
+	@RequestMapping(value="/", method=RequestMethod.GET)
+    public String showForm(RegUser reguser) {
+        return "index";
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String checkPersonInfo(@Valid RegUser reguser, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+        return "redirect:/test3";
+    }
 	@RequestMapping(method=RequestMethod.GET)
 	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 	model.addAttribute("name", name);
@@ -24,6 +46,11 @@ public class GreetingController {
 	model.addAttribute("name", name);
 	//System.out.println(name);
 	return"test2";}
+	@RequestMapping(value="/test3.html",method=RequestMethod.GET)
+	public String greeting4(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	model.addAttribute("name", name);
+	//System.out.println(name);
+	return"test3";}
 	
 }
 
