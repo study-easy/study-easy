94fd0c58b31cf9a5f5c676be1683a0b317aa947f
thar <connor.pecht@googlemail.com>
2015-07-02 15:37:29 +0200

    test

--- study-easy/src/main/java/system/System.java
+++ study-easy/src/main/java/system/System.java
@@ -14,7 +14,7 @@ public class System {
 	private static System system;
 	List<RegUser> regUserlist = new ArrayList<RegUser>();
 	List<Admin> adminList = new ArrayList<Admin>();
-		
+		//lel
 	public static System getSystem() {
 		if (system == null) {
 			system = new System();
