9d827fa9cab0aa4ba32ab8a212c1e66f50dd741d
SirBattle3 <tobias.94@gmx.de>
2015-07-05 17:40:26 +0200

    geil
    geiler

---
+++ study-easy/src/main/java/groupFunctions/Combat.java
@@ -0,0 +1,24 @@
+package groupFunctions;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Combat {
+	
+	
+	private Test test;
+	private Group challenger;
+	private Group opponent;
+	private Group corrector;
+	
+	public Combat(Group challenger, Group opponent, Group corrector){
+		this.challenger = challenger;
+		this.opponent = opponent;
+		this.corrector = corrector;
+		this.challenger.getCurrentCombats().add(this);
+		this.opponent.getCurrentCombats().add(this);
+		this.corrector.getCurrentCombats().add(this);
+		 //PENIS
+		
+	}
+}
