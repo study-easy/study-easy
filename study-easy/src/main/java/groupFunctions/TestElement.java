1514e3ded3360a94bb4b93b91c196ba94f431b83
Nicorobin21 <Jschamari@aol.com>
2015-07-05 15:46:06 +0200

    Combat, Tests, TestElement, Subject
    Jow

---
+++ study-easy/src/main/java/groupFunctions/TestElement.java
@@ -0,0 +1,13 @@
+package groupFunctions;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TestElement {
+
+	private Subject subject;
+	private String task;
+	private String answer;
+	
+	
+}
--- study-easy/src/main/java/groupFunctions/combat.java
+++ study-easy/src/main/java/groupFunctions/combat.java
@@ -1,5 +1,28 @@
 package groupFunctions;
 
-public class combat {
+import javax.persistence.Entity;
+
+@Entity
+public class Combat {
+	
+	
+	private Test test;
+	private Group challenger;
+	private Group opponent;
+	private Group corrector;
+	
+	public Combat(Group challenger, Group opponent, Group corrector){
+		this.challenger = challenger;
+		this.opponent = opponent;
+		this.corrector = corrector;
+		this.challenger.getCurrentCombats().add(this);
+		this.opponent.getCurrentCombats().add(this);
+		this.corrector.getCurrentCombats().add(this);
+		
+		
+	}
+	
+
+	
 
 }
