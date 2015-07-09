04d0210f1f2fa2d6f243d44bce036ee7d05efe6f
thar <connor.pecht@googlemail.com>
2015-07-09 23:18:44 +0200

    testklasse für mich erstellt, spielt damit rum so viel ihr wollt

---
+++ study-easy/src/main/java/org/hohenheim/testing.java
@@ -0,0 +1,12 @@
+package org.hohenheim;
+
+import system.Controller;
+
+public class testing {
+
+	public static void main(String[] args) {
+		Controller.getSystem();
+		System.out.println("xmlns");
+	}
+
+}
