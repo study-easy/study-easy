36ef1290c207301544b911b2dd2b7d0eec7d7868
thar <connor.pecht@googlemail.com>
2015-07-11 12:22:31 +0200

    test überflüssig

--- study-easy/src/main/java/org/hohenheim/testing.java
+++ study-easy/src/main/java/org/hohenheim/testing.java
@@ -23,29 +23,5 @@ public class testing {
 //		 String U2 = MU.addRegUser("lol", "abc", "ff@dd");
 //		
 //		 MU.listRegUsers();
-
-		Group g1 = new Group();
-		g1.setWins(12);
-		g1.setWinToLoss(4.5f);
-		g1.setWinStreak(1);
-		Group g2 = new Group();
-		g2.setWins(12);
-		g2.setWinToLoss(6f);
-		g2.setWinStreak(2);
-		Group g3 = new Group();
-		g3.setWins(12);
-		g3.setWinToLoss(2f);
-		g3.setWinStreak(1);
-		Leaderboard.getLeaderboard().addGroup(g2);
-		Leaderboard.getLeaderboard().addGroup(g1);
-		Leaderboard.getLeaderboard().addGroup(g3);
-		Group g4 = new Group();
-		g4.setWins(5);
-		g4.setWinStreak(2);
-		g4.setWinToLoss(0.5f);
-		Leaderboard.getLeaderboard().addGroup(g4);
-		for (Group group : Leaderboard.getLeaderboard().getBoard()) {
-			System.out.println(group.getWins() + " " + group.getWinToLoss() + " " + group.getWinStreak());
-		}
 	}
 }
