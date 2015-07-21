ad4a9c1b5f89433560498cc515967d321544a42a
SirBattle3 <tobias.94@gmx.de>
2015-07-21 13:30:47 +0200

    update
    update

--- study-easy/src/main/java/org/hohenheim/GreetingController.java
+++ study-easy/src/main/java/org/hohenheim/GreetingController.java
@@ -298,6 +298,7 @@ public class GreetingController {
 	@RequestMapping(value="/group", method=RequestMethod.GET)
 		public String showGroupInfo(@RequestParam (value="error", required=false, defaultValue= " ") String name, Model model) {
 		Group group = new Group();
+
 		model.addAttribute("groupName", "TestGruppe");
 		model.addAttribute("members", group.getUserList());
 		model.addAttribute("groupDescription", "Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.Das ist eine Testgruppe.");
