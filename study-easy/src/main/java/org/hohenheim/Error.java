02610a57ec68f85c3e585ffb745d7df35b935777
SirBattle3 <tobias.94@gmx.de>
2015-07-21 13:58:03 +0200

    d
    d

--- study-easy/src/main/java/org/hohenheim/Error.java
+++ study-easy/src/main/java/org/hohenheim/Error.java
@@ -1,5 +1,6 @@
 package org.hohenheim;
 
+import javax.validation.constraints.Size;
 
 /*
  * Klasse um ausschlieﬂlich Errormeldungen zu beinhalten welche durch eine Code
@@ -8,4 +9,32 @@ package org.hohenheim;
 public class Error {
 
 	 static String [] [] error = new String [3] [4];
+	 @Size(max=999)
+	 String name;
+	 @Size(max=9999999)
+	 String problem;
+	 @Size(max=9999999)
+	 String problemort;
+	 
+	 public String getName() {
+			return name;
+		}
+		public void setname(String name) {
+			this.name = name;
+		}
+	 
+	 public String getProblem() {
+			return problem;
+		}
+		public void setProblem(String problem) {
+			this.problem = problem;
+		}
+		public String getProblemort() {
+			return problemort;
+		}
+		public void setProblemort(String problemort) {
+			this.problemort = problemort;
+		}
+
+
 }
