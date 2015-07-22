9861d88835ff18ee7be72eece30a11c87380a5df
SirBattle3 <tobias.94@gmx.de>
2015-07-23 00:02:49 +0200

    update home.html
    d

--- study-easy/src/main/java/study_easy/org/hohenheim/GreetingController.java
+++ study-easy/src/main/java/study_easy/org/hohenheim/GreetingController.java
@@ -250,9 +250,10 @@ public class GreetingController {
 	 *
 	 */
 	@RequestMapping(value="/profilchange",method=RequestMethod.GET)
-	public String profilget(@RequestParam(value="name", required=false, defaultValue="Testname") String name, Model model) {
-	String error = "";
-	model.addAttribute("error", error); 
+	public String profilget(@RequestParam(value="user", required=true) String name, Model model) {
+	//String error = "";
+	//model.addAttribute("error", error);
+	System.out.println(name); 
 	RegUser reguser = new RegUser();
 	model.addAttribute("RegUser", reguser); 
 	model.addAttribute("name", name); 
@@ -280,8 +281,10 @@ public class GreetingController {
 	 *Da wo die Pinnwand ist
 	 */
 	@RequestMapping(value="/home",method=RequestMethod.GET)
-	public String home(@RequestParam(value="name", required=false, defaultValue="Mein Profil") String name, Model model) {
-	UserPinn userPinn = new UserPinn();
+	public String home(@RequestParam(value="user", required=true) String name, Model model) {
+	model.addAttribute("name", name);
+	model.addAttribute("user", name);
+		UserPinn userPinn = new UserPinn();
 	PinnwallElement element = new PinnwallElement();
 	Date date = new Date();
 	date.setDate(20);
@@ -289,8 +292,6 @@ public class GreetingController {
 	Group group = new Group();
 	PinnwallElement e = new PinnwallElement();
 	model.addAttribute("error", name);
-	model.addAttribute("name", name);
-
 	model.addAttribute("pinnwallOwner", "Hans");
 	model.addAttribute("pinnwallOwner", "Testuser 1");
 	model.addAttribute("PinnwallElement", element);
